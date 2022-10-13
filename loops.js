dc0d291182fe506e372fbfeea9a8568a3363f777
108nandita <108nandita@gmail.com>
2022-10-13 10:59:31 +0200

    Alles af

--- loops.js
+++ loops.js
@@ -7,6 +7,8 @@
 // ==========================================
 
 
+for ( let i=0; i<3; i++){
+    console.log("Hoera!")}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,9 +18,12 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
+for ( let i=0; i<4; i++){
+    console.log("loop...")}
+if (i=4){
+    console.log("klaar!")}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -35,6 +40,14 @@
 
 
 
+console.log("\n Opdracht 3 \n ");
+for (let i = 0; i < 16; i++) {
+    if (i%2 === 0){
+        console.log(i + " is even.");
+    } else {
+        console.log(i + " is oneven.");
+    }
+}
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
 // Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
@@ -50,4 +63,9 @@
 // >> 8
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++)
+    if (i>5) {console.log(">> " + i);}
+    else if (i>2) {console.log("> "+i);}
+    else {console.log(i+" ")}
 
